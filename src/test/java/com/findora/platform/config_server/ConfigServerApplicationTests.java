b4e89092eb673d8b9e888daf05931231efae18ab
Charuka974 <charuka974@gmail.com>
2026-08-17 08:36:30 +0530

    feat: initialize config project at root

---
+++ src/test/java/com/findora/platform/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findora.platform.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
